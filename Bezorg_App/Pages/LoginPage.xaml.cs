87e5a34c8dae089005365dd12746e5eb956deb8d
Keith Swaen <keithrandyswaen29@gmail.com>
2025-06-17 21:31:48 +0200

    Login pagina gemaakt

---
+++ Bezorg_App/Pages/LoginPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace Bezorg_App;
+
+public partial class LoginPage : ContentPage
+{
+    public LoginPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnLoginClicked(object sender, EventArgs e)
+    {
+        var password = PasswordEntry.Text;
+
+        if (password == "admin")
+        {
+            // Login succesvol, navigeer naar de hoofd- of homepagina
+            await DisplayAlert("Succes", "Inloggen gelukt!", "OK");
+
+            // Bijvoorbeeld naar MainPage navigeren:
+            Application.Current.MainPage = new MainPage();
+        }
+        else
+        {
+            ErrorLabel.Text = "Wachtwoord is incorrect.";
+            ErrorLabel.IsVisible = true;
+        }
+    }
+}

92460af8a9899fe58c889680f25090a59b842031
Keith Swaen <keithrandyswaen29@gmail.com>
2025-06-17 20:07:38 +0200

    Add project files.

---
+++ Bezorg_App/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Bezorg_App
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}

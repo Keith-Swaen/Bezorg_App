b772f9d5451f011e4081f5013b7663f167733dbc
LebVi <lebvince@hotmail.com>
2025-06-22 17:54:32 +0200

    Locatie functie

--- Bezorg_App/MainPage.xaml.cs
+++ Bezorg_App/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace Bezorg_App
+﻿using Microsoft.Maui.ApplicationModel;
+
+namespace Bezorg_App
 {
     public partial class MainPage : ContentPage
     {
@@ -26,6 +28,34 @@
             await Navigation.PushAsync(new BevestigBezorging());
         }
 
-    }
+        private async void OnLocationClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var location = await Geolocation.GetLastKnownLocationAsync();
 
+                if (location == null)
+                {
+                    location = await Geolocation.GetLocationAsync(new GeolocationRequest
+                    {
+                        DesiredAccuracy = GeolocationAccuracy.Medium,
+                        Timeout = TimeSpan.FromSeconds(10)
+                    });
+                }
+
+                if (location != null)
+                {
+                    await DisplayAlert("GPS Locatie", $"Latitude: {location.Latitude}\nLongitude: {location.Longitude}", "OK");
+                }
+                else
+                {
+                    await DisplayAlert("Fout", "Kon locatie niet bepalen.", "OK");
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Fout", $"Er is een fout opgetreden: {ex.Message}", "OK");
+            }
+        }
+    }
 }

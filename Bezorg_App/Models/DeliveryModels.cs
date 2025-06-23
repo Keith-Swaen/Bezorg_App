7b3f56927920bdc0a9e6876b052c7ae9a5b02343
LebVi <lebvince@hotmail.com>
2025-06-23 21:12:17 +0200

    Model aangemaakt voor deliverystate

---
+++ Bezorg_App/Models/DeliveryModels.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bezorg_App.Models
+{
+    public enum DeliveryStateEnum
+    {
+        Pending = 1,
+        InProgress = 2,
+        Completed = 3,
+        Cancelled = 4
+    }
+
+    public class DeliveryState
+    {
+        public int Id { get; set; }
+        public DeliveryStateEnum State { get; set; }
+        public DateTime DateTime { get; set; }
+        public int OrderId { get; set; }
+        public int DeliveryServiceId { get; set; }
+    }
+    public class DeliveryStatesResponse : List<DeliveryState>
+    {
+    }
+}

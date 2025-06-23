7c73b8576a2ac784fd918e49dddb4fd03ef3ba03
MilanZuyd <2405116janssen@zuyd.nl>
2025-06-24 00:24:42 +0200

    Handtekening + Volgorde met foto

--- Bezorg_App/MauiProgram.cs
+++ Bezorg_App/MauiProgram.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
+using Syncfusion.Maui.Core.Hosting; // <-- Required for Syncfusion
+using Syncfusion.Maui.SignaturePad;
 
 namespace Bezorg_App
 {
@@ -7,16 +9,18 @@ namespace Bezorg_App
         public static MauiApp CreateMauiApp()
         {
             var builder = MauiApp.CreateBuilder();
+
             builder
                 .UseMauiApp<App>()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
+                })
+                .ConfigureSyncfusionCore(); // <-- Register Syncfusion here
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();

2615cd2fbdcc8170559dda0a5c5a70e05a01f5b4
Keith Swaen <keithrandyswaen29@gmail.com>
2025-06-23 22:49:50 +0200

    VIngerprint scan functie toegevoegd voor in te loggen

--- Bezorg_App/Pages/LoginPage.xaml.cs
+++ Bezorg_App/Pages/LoginPage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
-using Microsoft.Maui.Controls;
+﻿using Microsoft.Maui.Controls;
+using Plugin.Fingerprint;
+using Plugin.Fingerprint.Abstractions;
+using System;
 
 namespace Bezorg_App;
 
@@ -12,7 +14,7 @@ public partial class LoginPage : ContentPage
 
     private async void OnLoginClicked(object sender, EventArgs e)
     {
-        var password = PasswordEntry.Text?.Trim();  
+        var password = PasswordEntry.Text?.Trim();
 
         if (password == "admin")
         {
@@ -25,6 +27,37 @@ public partial class LoginPage : ContentPage
         {
             ErrorLabel.Text = "Wachtwoord is incorrect.";
             ErrorLabel.IsVisible = true;
+        }
+    }
+
+    private async void OnFingerprintLoginClicked(object sender, EventArgs e)
+    {
+        var availability = await CrossFingerprint.Current.IsAvailableAsync();
+
+        if (availability)
+        {
+            var authRequest = new AuthenticationRequestConfiguration("Bevestigen", "Gebruik uw vingerprint om in te loggen")
+            {
+                CancelTitle = "Annuleer"
+            };
+
+            var authResult = await CrossFingerprint.Current.AuthenticateAsync(authRequest);
+
+            if (authResult.Authenticated)
+            {
+                await DisplayAlert("Succes", "U bent ingelogd via vingerprint!", "OK");
+
+                // Navigeer naar MainPage (vervangt huidige pagina)
+                Application.Current.MainPage = new MainPage();
+            }
+            else
+            {
+                await DisplayAlert("Mislukt", "Vingerprint niet herkend of geannuleerd.", "OK");
+            }
+        }
+        else
+        {
+            await DisplayAlert("Niet beschikbaar", "Biometrische authenticatie is niet beschikbaar op dit apparaat.", "OK");
         }
     }
 }

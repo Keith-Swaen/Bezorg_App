ea37fdf38f3ee06a57addb9f89de73807bb7eaaf
Lars <kleijnen.lars@gmail.com>
2025-06-28 18:55:20 +0200

    Kloktijden pagina inhoud toegevoegd + on click functies

--- Bezorg_App/Views/KloktijdenPage.xaml.cs
+++ Bezorg_App/Views/KloktijdenPage.xaml.cs
@@ -1,9 +1,66 @@
+//using Windows.UI.Notifications;
+
 namespace Bezorg_App.Views;
 
 public partial class KloktijdenPage : ContentPage
 {
-	public KloktijdenPage()
-	{
-		InitializeComponent();
-	}
+    private bool _isClockedIn;
+    private bool _isOnBreak;
+    private DateTime _clockInTime;
+    private DateTime? _breakStartTime;
+    private TimeSpan _totalWorkTime;
+    private Timer _workTimeTimer;
+
+    public KloktijdenPage()
+    {
+        InitializeComponent();
+        LoadState();
+        UpdateStatus();
+        StartWorkTimeTimer();
+    }
+
+    private void LoadState()
+    {
+       
+    }
+
+    private void UpdateStatus()
+    {
+       
+    }
+
+    private void StartWorkTimeTimer()
+    {
+       
+    }
+
+    private void UpdateWorkTime()
+    {
+       
+    }
+
+    private async void OnClockInClicked(object sender, EventArgs e)
+    {
+       
+    }
+
+    private async void OnClockOutClicked(object sender, EventArgs e)
+    {
+       
+    }
+
+    private async void OnStartBreakClicked(object sender, EventArgs e)
+    {
+       
+    }
+
+    private async void OnEndBreakClicked(object sender, EventArgs e)
+    {
+       
+    }
+
+    private async Task ScheduleBreakNotification(int breakMinutes)
+    {
+        
+    }
 }
